b80008bb482417412323a46a0b4d63ce7416a10c
MaliaLabor <malia.labor@hotmail.com>
2023-01-27 11:26:02 -0700

    :recycle: Conform to libhal

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # libstm32f10x
 
-[![.github/workflows/presubmit.yml](https://github.com/SJSU-Dev2/libstm32f10x/actions/workflows/presubmit.yml/badge.svg)](https://github.com/SJSU-Dev2/libstm32f10x/actions/workflows/presubmit.yml)
+[![✅ Checks](https://github.com/libhal/libhal-stm32f10x/actions/workflows/ci.yml/badge.svg)](https://github.com/libhal/libhal-stm32f10x/actions/workflows/ci.yml)
+[![Coverage](https://libhal.github.io/libhal-stm32f10x/coverage/coverage.svg)](https://libhal.github.io/libhal-stm32f10x/coverage/)
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/b084e6d5962d49a9afcb275d62cd6586)](https://www.codacy.com/gh/libhal/libhal-stm32f10x/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=libhal/libhal-stm32f10x&amp;utm_campaign=Badge_Grade)
+[![GitHub stars](https://img.shields.io/github/stars/libhal/libhal-stm32f10x.svg)](https://github.com/libhal/libhal-stm32f10x/stargazers)
+[![GitHub forks](https://img.shields.io/github/forks/libhal/libhal-stm32f10x.svg)](https://github.com/libhal/libhal-stm32f10x/network)
+[![GitHub issues](https://img.shields.io/github/issues/libhal/libhal.svg)](https://github.com/libhal/libhal/issues)
+[![Latest Version](https://libhal.github.io/libhal-stm32f10x/latest_version.svg)](https://github.com/libhal/libhal-stm32f10x/blob/main/conanfile.py)
+[![ConanCenter Version](https://repology.org/badge/version-for-repo/conancenter/libhal-stm32f10x.svg)](https://conan.io/center/libhal-stm32f10x)
 
 libembeddedhal peripheral libraries for stm32f10x series of devices.
--- conanfile.py
+++ conanfile.py
@@ -1,35 +1,109 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.files import copy
+from conan.tools.layout import basic_layout
+from conan.tools.build import check_min_cppstd
+from conan.errors import ConanInvalidConfiguration
+import os
 
+required_conan_version = ">=1.50.0"
 
-class libstm32f10x_conan(ConanFile):
-    name = "libstm32f10x"
+class libhalSTM32F10x_conan(ConanFile):
+    name = "libhal-stm32f10x"
     version = "0.0.1"
-    license = "Apache License Version 2.0"
-    author = "Khalil Estell"
-    url = "https://github.com/SJSU-Dev2/libstm32f10x"
-    description = "Drivers for the stm32f10x series of microcontrollers using libembeddedhal's abstractions."
-    topics = ("peripherals", "hardware")
-    settings = "os", "compiler", "arch", "build_type"
-    generators = "cmake_find_package"
-    exports_sources = "include/*", "CMakeLists.txt", "tests/*"
+    license = "Apache-2.0"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://libhal.github.io/libhal-stm32f10x"
+    description = ("Drivers for the stm32f10x series of microcontrollers using "
+                    "libhal's abstractions.")
+    topics = ("ARM", "microcontroller", "peripherals", "hardware", "stm32f10x")
+    settings = "compiler"
+    exports_sources = ("include/*", "linkers/*", "LICENSE")
     no_copy_source = True
 
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-        if self.should_test:
-            self.run("ctest -j %s --output-on-failure" % tools.cpu_count())
-
-    def package(self):
-        self.copy("*.hpp")
-
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
+    
+    @property
+    def _min_cppstd(self):
+        return "20"
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "11",
+            "clang": "14",
+            "apple-clang": "14.0.0"
+        }
 
     def requirements(self):
-        self.requires("libarmcortex/0.0.1@")
+        self.requires("libhal/0.3.3@")
+        self.requires("libhal-util/0.3.7@")
+        self.requires("libhal-armcortex/0.3.8@")
         self.requires("ring-span-lite/0.6.0")
 
-    def build_requirements(self):
-        self.test_requires("boost-ext-ut/1.1.8@")
+    def validate(self):
+        if self.settings.get_safe("compiler.cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+
+        def lazy_lt_semver(v1, v2):
+                lv1 = [int(v) for v in v1.split(".")]
+                lv2 = [int(v) for v in v2.split(".")]
+                min_length = min(len(lv1), len(lv2))
+                return lv1[:min_length] < lv2[:min_length]
+
+        compiler = str(self.settings.compiler)
+        version = str(self.settings.compiler.version)
+        minimum_version = self._compilers_minimum_version.get(compiler, False)
+
+        if minimum_version and lazy_lt_semver(version, minimum_version):
+            raise ConanInvalidConfiguration(
+                f"{self.name} {self.version} requires C++{self._min_cppstd}, which your compiler ({compiler}-{version}) does not support")
+
+    def layout(self):
+        basic_layout(self)
+
+    def package(self):
+        copy(self, "LICENSE", dst=os.path.join(
+            self.package_folder, "licenses"),  src=self.source_folder)
+        copy(self, "*.h", dst=os.path.join(self.package_folder, "include"),
+             src=os.path.join(self.source_folder, "include"))
+        copy(self, "*.hpp", dst=os.path.join(self.package_folder,
+             "include"), src=os.path.join(self.source_folder, "include"))
+        copy(self, "*.ld", dst=os.path.join(self.package_folder,
+             "linkers"), src=os.path.join(self.source_folder, "linkers"))
+
+    def package_info(self):
+        requirements_list = ["libhal::libhal",
+                             "libhal-util::libhal-util",
+                             "libhal-armcortex::libhal-armcortex",
+                             "ring-span-lite::ring-span-lite"]
+
+        m4_architecture_flags = [
+            "-mcpu=cortex-m4",
+            "-mthumb",
+            "-mfloat-abi=soft"
+        ]
+
+        linker_path = os.path.join(self.package_folder, "linkers")
+
+        self.cpp_info.set_property("cmake_file_name", "libhal-stm32f10x")
+        self.cpp_info.set_property("cmake_find_mode", "both")
+
+        self.cpp_info.components["stm32f10x"].set_property(
+            "cmake_target_name",  "libhal::stm32f10x")
+        self.cpp_info.components["stm32f10x"].exelinkflags.append(
+            "-L" + linker_path)
+        self.cpp_info.components["stm32f10x"].requires = requirements_list
+
+        def create_component(self, component, flags):
+            link_script = "-Tlibhal-stm32f10x/" + component + ".ld"
+            component_name = "libhal::" + component
+            self.cpp_info.components[component].set_property(
+                "cmake_target_name", component_name)
+            self.cpp_info.components[component].requires = ["stm32f10x"]
+            self.cpp_info.components[component].exelinkflags.append(link_script)
+            self.cpp_info.components[component].exelinkflags.extend(flags)
+            self.cpp_info.components[component].cflags = flags
+            self.cpp_info.components[component].cxxflags = flags
+
+        create_component(self, "stm32f103", m4_architecture_flags)
